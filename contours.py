32c81a776ab094fc620ec90455a9a210a80515cd
renyueyy <94286807+renyueyy@users.noreply.github.com>
2023-11-06 15:11:13 +0800

    Add files via upload

--- contours.py
+++ contours.py
@@ -1,6 +1,7 @@
 import cv2 as cv
 import numpy as np
 import os
+import math
 
 def black_background(input_path,output_path):
     """
@@ -16,7 +17,7 @@ def black_background(input_path,output_path):
     for dir in dir_list:
         dir_input = input_path+'/'+dir
         pic_list = os.listdir(dir_input)
-        dir_output = output_path+'/'+dir
+        dir_output = os.path.join(output_path,dir)
         if not os.path.exists(dir_output):  # 建立输出文件夹
             os.makedirs(dir_output)
 
@@ -57,7 +58,9 @@ def black_background(input_path,output_path):
             (cnts, add_image) = cv.findContours(add, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)  # 轮廓检测
             gaussedge = cv.drawContours(contour, cnts, -1, (0, 255, 0), 2)  # 绘制轮廓
             blur = cv.medianBlur(gaussedge, 21)  # 中值滤波平滑边缘
-            cv.imwrite(dir_output + f"/{count}.jpg", blur)
+            #print(dir_output)
+            print(os.path.join(dir_output,f"/{count}.jpg"))
+            cv.imwrite(dir_output+"/"+f"/{count}.jpg", blur)
 
             # 计算轮廓面积
             areas = []
@@ -128,7 +131,7 @@ def other_background(input_path,output_path):
             (cnts, _) = cv.findContours(blur, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)  # 轮廓检测
             gaussedge = cv.drawContours(contour, cnts, -1, (0, 0, 255), 2)  # 绘制轮廓
             blur = cv.medianBlur(gaussedge, 21)  # 中值滤波平滑边缘
-            cv.imwrite(dir_output + f"/{count}.jpg", blur)
+            cv.imwrite(os.path.join(dir_output,"{}.jpg".format(count)), blur)
 
             # 计算轮廓面积
             areas = []
@@ -144,7 +147,7 @@ def other_background(input_path,output_path):
     return
 
 
-def white_background(input_path,output_path):
+def white_background(input_path,output_path, option, area_average):
     """
     #黑色背景下图片轮廓的提取
     :param input_division: 分割后图片的文件夹（只写到文件夹，每个文件夹内的图片在函数里循环读入）
@@ -166,43 +169,39 @@ def white_background(input_path,output_path):
         for m in pic_list:
             count +=1
             img = cv.imread(dir_input+'/'+m)  # 读入图片
-
-            # 原图片腐蚀处理
-            # 创建核结构
             hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)  # 转换为hsv格式
             kenel = np.ones((10, 10), np.uint8)  # 卷积核
-            kenel2 = np.ones((20, 20), np.uint8)
+
             img2 = cv.GaussianBlur(hsv, (5, 5), 1)  # 高斯模糊
-            b, g, r = cv.split(img2)  # 单通道处理
-            # ret,image_green = cv.threshold(g, 65, 255, cv.THRESH_BINARY)#固定阈值二值化
-            ret, image_green = cv.threshold(g, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
-            green_gauss = cv.GaussianBlur(image_green, (5, 5), 0)  # 高斯模糊
-            close_green = cv.morphologyEx(green_gauss, cv.MORPH_CLOSE, kenel2)  # 闭运算
+            b, g, r = cv.split(img2)  # 对高斯模糊后对图片进行单通道分离
+            # ret, image_green = cv.threshold(g, 65, 255, cv.THRESH_BINARY)  # 对单通道图片进行固定阈值二值化处理
+            ret, image_green = cv.threshold(g, 10, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
+            green_gauss = cv.GaussianBlur(image_green, (5, 5), 0)
+            close_green = cv.morphologyEx(green_gauss, cv.MORPH_CLOSE, kenel)
 
-            height, width, deep = img.shape
-            dst = np.zeros((height, width, 3), np.uint8)
-            for i in range(height):  # 色彩反转
-                for j in range(width):
-                    b, g, r = img[i, j]
-                    dst[i, j] = (255 - b, 255 - g, 255 - r)
+            contour = img.copy()
+            (cnts, _) = cv.findContours(close_green, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)  # 轮廓检测
+            #gaussedge = cv.drawContours(contour, cnts, -1, (0, 255, 0), 2)  # 绘制轮廓
 
-            b, g, r = cv.split(dst)
+            blur = cv.medianBlur(close_green, 31)  # 中值滤波平滑边缘
 
-            # 二值化处理
-            # ret,image_g = cv.threshold(g,170, 255, cv.THRESH_BINARY)
-            ret, image_g = cv.threshold(g, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)  # OSTU阈值
-            g_gauss = cv.GaussianBlur(image_g, (5, 5), 0)  # 高斯模糊
+            if option == "y":
+                area_img = 0
+                for n in cnts:
+                    area_img += cv.contourArea(n)
 
-            add = cv.add(g_gauss, close_green)  # hsv和
-            # add = cv.add(blue_gauss,green_gauss)
-            close_add = cv.morphologyEx(add, cv.MORPH_CLOSE, kenel2)
+                k2 = float(area_average) / float(area_img)  # 求resize面积的倍数
+                k = math.sqrt(k2)  # 开根号，边长更改的倍数是面积的开根号
 
-            # 提取轮廓
-            contour = add.copy()
-            (cnts, add_image) = cv.findContours(add, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)  # 轮廓检测
-            gaussedge = cv.drawContours(contour, cnts, -1, (0, 255, 0), 2)  # 绘制轮廓
-            blur = cv.medianBlur(gaussedge, 21)  # 中值滤波平滑边缘
-            cv.imwrite(dir_output + f"/{count}.jpg", blur)
+                h, w = blur.shape  # 求resize面积后对边长
+                w_s = int(w * k)
+                h_s = int(h * k)
+                # img_resize = cv.resize(img, (w_s, h_s), fx=1, fy=1, interpolation=cv.INTER_AREA)
+                img_resize = cv.resize(blur, (w_s, h_s), fx=1, fy=1, interpolation=cv.INTER_LINEAR)  # 重置图片
+                cv.imwrite(os.path.join(dir_output, f"/{count}.jpg"), img_resize)
+
+            elif option == "n":
+                cv.imwrite(os.path.join(dir_output,"{}.jpg".format(m)), blur)
 
             # 计算轮廓面积
             areas = []
@@ -211,8 +210,8 @@ def white_background(input_path,output_path):
                 area += cv.contourArea(n)
             areas = np.append(areas, area)
         average = np.mean(areas)
-        print(dir,':',average)
-        average_area = np.append(average_area,average)
+        print(dir, ':', average)
+        average_area = np.append(average_area, average)
     average_all = np.mean(average_area)
-    print('all_average',average_all)
+    print('all_average', average_all)
     return
--- division.py
+++ division.py
@@ -2,6 +2,36 @@ import cv2 as cv
 import numpy as np
 import os
 
+def distortion(img):
+    # Load the image and convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # find Harris corners as we did in the previous blog
+    gray = np.float32(gray)
+    dst = cv2.cornerHarris(gray, 2, 3, 0.04)
+    dst = cv2.dilate(dst, None)
+    ret, dst = cv2.threshold(dst, 0.01 * dst.max(), 255, 0)
+    dst = np.uint8(dst)
+
+    # find centroids
+    ret, labels, stats, centroids = cv2.connectedComponentsWithStats(dst)
+
+    # define the criteria to stop. We stop it after a specified number of iterations
+    # or a certain accuracy is achieved, whichever occurs first.
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001)
+
+    # Refine the corners using cv2.cornerSubPix()
+    corners = cv2.cornerSubPix(gray, np.float32(centroids), (5, 5), (-1, -1), criteria)
+
+    # To display, first convert the centroids and corners to integer
+    centroids = np.int0(centroids)
+    corners = np.int0(corners)
+
+    img[centroids[:, 1], centroids[:, 0]] = [0, 0, 255]
+    img[corners[:, 1], corners[:, 0]] = [0, 255, 0]
+
+    return img
+
+
 def black_division(input_path, output_path):
     """
         :param input_path: 输入文件的路径及名称
@@ -14,7 +44,8 @@ def black_division(input_path, output_path):
 
     for m in name_list:
         pic_path = os.path.join(input_path, m)
-        image = cv.imread(pic_path)  # 读入图片
+        img = cv.imread(pic_path)  # 读入图片
+        image = distortion(img)
 
         pic_out_path = output_path+'/'+m+'/'#输出文件路径
         if not os.path.exists(pic_out_path):
@@ -65,7 +96,7 @@ def black_division(input_path, output_path):
             M2 = cv.getRotationMatrix2D((x1, y1), rect[2], 1)
             rotated_image = cv.warpAffine(image, M2, (w1 * 2, h1 * 2))
             rotated_canvas = rotated_image[y - margin:y + h + margin + 1, x - margin:x + w + margin + 1]
-            cv.imwrite(pic_out_path + "{}.jpg".format(count), rotated_canvas)  # 保存切割图片
+            cv.imwrite(os.path.join(pic_out_path, "{}.jpg".format(count)), rotated_canvas)  # 保存切割图片
 
         # print("jujuba #{}".format(count))
         #cv.imwrite(pic_out_path + "rect.jpg", draw_rect)  # 绘制矩形在原图上
@@ -84,7 +115,8 @@ def blue_division(input_path, output_path):
     for m in name_list:
         #print(m)
         pic_path = os.path.join(input_path, m)
-        img = cv.imread(pic_path)# 读入图片
+        image = cv.imread(pic_path)# 读入图片
+        img = distortion(image)
         height, width, deep = img.shape  # 对白色北京
         dst = np.zeros((height, width, 3), np.uint8)
         for i in range(height):  # 色彩反转
@@ -93,6 +125,7 @@ def blue_division(input_path, output_path):
                 dst[i, j] = (255 - b, 255 - g, 255 - r)
 
         pic_out_path = output_path+'/'+m+'/'#输出文件路径
+        pic_out_path = os.path.join(output_path,m)
         if not os.path.exists(pic_out_path):
             os.makedirs(pic_out_path)
 
@@ -138,7 +171,7 @@ def blue_division(input_path, output_path):
             M2 = cv.getRotationMatrix2D((x1, y1), rect[2], 1)
             rotated_image = cv.warpAffine(img, M2, (w1 * 2, h1 * 2))
             rotated_canvas = rotated_image[y - margin:y + h + margin + 1, x - margin:x + w + margin + 1]
-            cv.imwrite(pic_out_path + "{}.jpg".format(count), rotated_canvas)  # 保存切割图片
+            cv.imwrite(os.path.join(pic_out_path,"{}.jpg".format(count)), rotated_canvas)  # 保存切割图片
 
         #print("jujuba #{}".format(count))
         #cv.imwrite(pic_out_path + "rect.jpg", draw_rect)  # 绘制矩形在原图上
@@ -149,60 +182,74 @@ def white_division(input,output):
     :param output:输出文件的路径，不需要名称，是输出文件夹
     :return:
     """
-    img = cv.imread(input)#读入图片
+    name_list = os.listdir(input)  # 获得图片名称列表，作为循环列表
+    # print(name_list)
 
-    hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)#转换为hsv格式
-    kenel = np.ones((10, 10), np.uint8)#卷积核
-    kenel2 = np.ones((20, 20), np.uint8)#定义卷积核
-    img2 = cv.GaussianBlur(hsv, (5, 5), 1)#高斯模糊
-    b, g, r = cv.split(img2)#对高斯模糊后对图片进行单通道分离
-    ret, image_green = cv.threshold(g, 65, 255, cv.THRESH_BINARY)#对单通道图片进行固定阈值二值化处理
-    green_gauss = cv.GaussianBlur(image_green, (5, 5), 0)
-    close_green = cv.morphologyEx(green_gauss, cv.MORPH_CLOSE, kenel2)
+    for m in name_list:
+        pic_path = os.path.join(input, m)
+        image = cv.imread(pic_path)  # 读入图片
+        img = distortion(image)
+        print(m)
 
-    height, width, deep = img.shape#对白色北京
-    dst = np.zeros((height, width, 3), np.uint8)
-    for i in range(height):  # 色彩反转
-        for j in range(width):
-            b, g, r = img[i, j]
-            dst[i, j] = (255 - b, 255 - g, 255 - r)
+        pic_out_path = os.path.join(output,m)#输出文件路径
+        if not os.path.exists(pic_out_path):
+            os.makedirs(pic_out_path)
 
-    b, g, r = cv.split(dst)
+        hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)  # 转换为hsv格式
+        kenel = np.ones((10, 10), np.uint8)  # 卷积核
+        kenel2 = np.ones((20, 20), np.uint8)  # 定义卷积核
+        img2 = cv.GaussianBlur(hsv, (5, 5), 1)  # 高斯模糊
+        b, g, r = cv.split(img2)  # 对高斯模糊后对图片进行单通道分离
+        ret, image_green = cv.threshold(g, 65, 255, cv.THRESH_BINARY)  # 对单通道图片进行固定阈值二值化处理
+        green_gauss = cv.GaussianBlur(image_green, (5, 5), 0)
+        close_green = cv.morphologyEx(green_gauss, cv.MORPH_CLOSE, kenel2)
 
-    # 轮廓提取
-    ret, image_g = cv.threshold(g, 170, 255, cv.THRESH_BINARY)
-    g_gauss = cv.GaussianBlur(image_g, (5, 5), 0)
-    close_g = cv.morphologyEx(g_gauss, cv.MORPH_CLOSE, kenel)
+        height, width, deep = img.shape  # 对白色北京
+        dst = np.zeros((height, width, 3), np.uint8)
+        for i in range(height):  # 色彩反转
+            for j in range(width):
+                b, g, r = img[i, j]
+                dst[i, j] = (255 - b, 255 - g, 255 - r)
 
-    add = cv.add(close_g, close_green)
-    close_add = cv.morphologyEx(add, cv.MORPH_CLOSE, kenel2)
-    contour = img.copy()
+        b, g, r = cv.split(dst)
 
-    (cnts, _) = cv.findContours(close_add, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)  # 轮廓检测
-    cv.drawContours(contour, cnts, -1, (0, 255, 0), 2)  # 绘制轮廓
-    #gaussedge = cv.Canny(close_add, 0, 50)
+        # 轮廓提取
+        ret, image_g = cv.threshold(g, 170, 255, cv.THRESH_BINARY)
+        g_gauss = cv.GaussianBlur(image_g, (5, 5), 0)
+        close_g = cv.morphologyEx(g_gauss, cv.MORPH_CLOSE, kenel)
 
-    count = 0  # 图像个数
-    margin = 10  # 裁剪边距
-    draw_rect = img.copy()
-    for i, contour in enumerate(cnts):
-        area = cv.contourArea(contour)  # 计算包围形状的面积
-        if area < 30000 or area > 380000:  # 过滤面积，选择>30000,<150000的对象
-            continue
-        count += 1
-        x, y, w, h = cv.boundingRect(contour)  # 确定正外接矩形左下角坐标（x,y）、宽w和高h
-        box = np.array([[x, y], [x + w, y],
-                        [x + w, y + h], [x, y + h]])  # 确定正矩形四个角的坐标
-        cv.drawContours(draw_rect, [box], 0, (255, 0, 0), 2)  # 绘制外接矩形
-        h1, w1 = img.shape[:2]
-        x1 = int(x + w / 2)
-        y1 = int(y + h / 2)  # 获得图片中心点（x1，y1）
-        rect = ((x1, y1), (w, h), 0)
-        M2 = cv.getRotationMatrix2D((x1, y1), rect[2], 1)
-        rotated_image = cv.warpAffine(img, M2, (w1 * 2, h1 * 2))
-        rotated_canvas = rotated_image[y - margin:y + h + margin + 1, x - margin:x + w + margin + 1]
-        cv.imwrite(output + "{}.jpg".format(count), rotated_canvas)  # 保存切割图片
-    #cv.imwrite(output+"rect.jpg", draw_rect)
+        add = cv.add(close_g, close_green)
+        close_add = cv.morphologyEx(add, cv.MORPH_CLOSE, kenel2)
+        contour = img.copy()
+
+        (cnts, _) = cv.findContours(close_add, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)  # 轮廓检测
+        cv.drawContours(contour, cnts, -1, (0, 255, 0), 2)  # 绘制轮廓
+        # gaussedge = cv.Canny(close_add, 0, 50)
+
+        count = 0  # 图像个数
+        margin = 10  # 裁剪边距
+        draw_rect = img.copy()
+        for i, contour in enumerate(cnts):
+            area = cv.contourArea(contour)  # 计算包围形状的面积
+            if area < 30000 or area > 180000:  # 过滤面积，选择>30000,<150000的对象
+                continue
+            count += 1
+            x, y, w, h = cv.boundingRect(contour)  # 确定正外接矩形左下角坐标（x,y）、宽w和高h
+            box = np.array([[x, y], [x + w, y],
+                            [x + w, y + h], [x, y + h]])  # 确定正矩形四个角的坐标
+            cv.drawContours(draw_rect, [box], 0, (255, 0, 0), 2)  # 绘制外接矩形
+            h1, w1 = img.shape[:2]
+            x1 = int(x + w / 2)
+            y1 = int(y + h / 2)  # 获得图片中心点（x1，y1）
+            rect = ((x1, y1), (w, h), 0)
+            M2 = cv.getRotationMatrix2D((x1, y1), rect[2], 1)
+            rotated_image = cv.warpAffine(img, M2, (w1 * 2, h1 * 2))
+            rotated_canvas = rotated_image[y - margin:y + h + margin + 1, x - margin:x + w + margin + 1]
+            try:
+                cv.imwrite(os.path.join(pic_out_path, "{}.jpg".format(count)), rotated_canvas)  # 保存切割图片
+            except:
+                print(count)
+        # cv.imwrite(output+"rect.jpg", draw_rect)
     return img
 
 
